2c2ef731af8e25f9ee0102deee175bdf6371d4f8
bivex <209128140+bivex@users.noreply.github.com>
2025-12-23 21:17:56 +0200

    feat: update demo pages with latest component versions
    - Update animated hero demo page
    - Update modern SaaS hero demo page
    - Update modern SaaS wireframe 2025 demo page
    - Update main demo page with latest components

--- src/app/[locale]/demo/page.tsx
+++ src/app/[locale]/demo/page.tsx
@@ -1,4 +1,19 @@
 /**
+ * Copyright (c) 2025 Bivex
+ *
+ * Author: Bivex
+ * Available for contact via email: support@b-b.top
+ * For up-to-date contact information:
+ * https://github.com/bivex
+ *
+ * Created: 2025-12-23T19:01:02
+ * Last Updated: 2025-12-23T19:08:18
+ *
+ * Licensed under the MIT License.
+ * Commercial licensing available upon request.
+ */
+
+/**
  * Demo page for the Testimonials component
  * Available at /demo route
  */
