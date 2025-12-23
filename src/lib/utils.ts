5de2fcdc978f6f5039408427eb9480537b30c8e0
bivex <209128140+bivex@users.noreply.github.com>
2025-12-23 21:18:24 +0200

    feat: add lib utilities and finalize header component
    - Add src/lib/utils.ts with cn utility function for className merging
    - Finalize header component with all navigation and styling improvements
    - Complete the component integration with proper utility functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2025 Bivex
+ *
+ * Author: Bivex
+ * Available for contact via email: support@b-b.top
+ * For up-to-date contact information:
+ * https://github.com/bivex
+ *
+ * Created: 2025-12-23T19:05:51
+ * Last Updated: 2025-12-23T19:05:55
+ *
+ * Licensed under the MIT License.
+ * Commercial licensing available upon request.
+ */
+
+import type { ClassValue } from 'clsx';
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
